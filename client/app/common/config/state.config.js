93adba13fd5eb929689dc7afd032df8b03be7bd4
gkochakidze <gkochakidze@gmail.com>
2015-10-30 14:39:55 +0400

    clean code in state.config.js

--- client/app/common/config/state.config.js
+++ client/app/common/config/state.config.js
@@ -2,19 +2,5 @@
 
 export default ($rootScope, $state, $mdDialog) => {
   'ngInject';
-
-  $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) => {
-    switch (error) {
-      case 'unauthorizedAdmin':
-        $state.go('main.loginAdmin');
-        break;
-      default:
-        $state.go('main');
-        break;
-    }
-  });
-
-  $rootScope.$on('$stateChangeStart', (event, toState, toParams, fromState, fromParams, error) => {
-    $mdDialog.hide();
-  });
+  $rootScope.$on('$stateChangeStart', () => $mdDialog.hide());
 }
