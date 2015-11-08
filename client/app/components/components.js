505f5c09f8a90df3761d12faf8e885062171ab1c
GiorgiSheshaberidze <gshes11@freeuni.edu.ge>
2015-11-08 17:28:44 +0400

    main

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -2,52 +2,52 @@
 
 import Main from './main/main';
 //inject:import.main
-import about from './main/about/about';
-import demographics from './main/demographics/demographics';
-import home from './main/home/home';
-import sitemap from './main/sitemap/sitemap';
+import mainabout from './main/about/about';
+import maindemographics from './main/demographics/demographics';
+import mainhome from './main/home/home';
+import mainsitemap from './main/sitemap/sitemap';
 //endinject
 
 import Admin from './admin/admin';
 //inject:import.admin
-import aboutUs from './admin/aboutUs/aboutUs';
-import addIndicator from './admin/addIndicator/addIndicator';
-import addPartner from './admin/addPartner/addPartner';
-import addPublication from './admin/addPublication/addPublication';
-import contact from './admin/contact/contact';
-import demography from './admin/demography/demography';
-import editIndicator from './admin/editIndicator/editIndicator';
-import editPublication from './admin/editPublication/editPublication';
-import indicators from './admin/indicators/indicators';
-import landingPage from './admin/landingPage/landingPage';
-import partners from './admin/partners/partners';
-import photos from './admin/photos/photos';
-import publications from './admin/publications/publications';
+import adminaboutUs from './admin/aboutUs/aboutUs';
+import adminaddIndicator from './admin/addIndicator/addIndicator';
+import adminaddPartner from './admin/addPartner/addPartner';
+import adminaddPublication from './admin/addPublication/addPublication';
+import admincontact from './admin/contact/contact';
+import admindemography from './admin/demography/demography';
+import admineditIndicator from './admin/editIndicator/editIndicator';
+import admineditPublication from './admin/editPublication/editPublication';
+import adminindicators from './admin/indicators/indicators';
+import adminlandingPage from './admin/landingPage/landingPage';
+import adminpartners from './admin/partners/partners';
+import adminphotos from './admin/photos/photos';
+import adminpublications from './admin/publications/publications';
 //endinject
 
 export default angular.module('app.components', [
   Main.name,
   //inject:ngmodule.main
-  about.name,
-  demographics.name,
-  home.name,
-  sitemap.name,
+  mainabout.name,
+  maindemographics.name,
+  mainhome.name,
+  mainsitemap.name,
   //endinject
 
   Admin.name,
   //inject:ngmodule.admin
-  aboutUs.name,
-  addIndicator.name,
-  addPartner.name,
-  addPublication.name,
-  contact.name,
-  demography.name,
-  editIndicator.name,
-  editPublication.name,
-  indicators.name,
-  landingPage.name,
-  partners.name,
-  photos.name,
-  publications.name,
+  adminaboutUs.name,
+  adminaddIndicator.name,
+  adminaddPartner.name,
+  adminaddPublication.name,
+  admincontact.name,
+  admindemography.name,
+  admineditIndicator.name,
+  admineditPublication.name,
+  adminindicators.name,
+  adminlandingPage.name,
+  adminpartners.name,
+  adminphotos.name,
+  adminpublications.name,
   //endinject
 ]);
