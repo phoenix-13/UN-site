c0ac0b0a2a60e1af6cde17fbbf0585fdd7e25554
GiorgiKochakidze <gkochakidze@gmail.com>
2015-11-11 21:07:07 +0400

    aboutUs, LandingPage, Partners

--- client/app/common/config/config.js
+++ client/app/common/config/config.js
@@ -4,10 +4,12 @@ import routerConfig from './router.config';
 import stateConfig from './state.config';
 import restangularConfig from './restangular.config';
 import materialConfig from './material.config';
+import editorConfig from './editor.config';
 
 export default angular
   .module('config', [])
   .config(routerConfig)
   .config(restangularConfig)
   .config(materialConfig)
+  .config(editorConfig)
   .run(stateConfig);
