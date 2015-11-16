d9b7501d14b982a8629cb3399900e0d42c612402
GiorgiKochakidze <gkochakidze@gmail.com>
2015-11-16 11:54:26 +0400

    search navigation

--- client/app/common/header/header.controller.js
+++ client/app/common/header/header.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
 export default class {
-  constructor(ArticleResource) {
+  constructor($state, ArticleResource) {
     'ngInject';
+    this.$state = $state;
     this.ArticleResource= ArticleResource;
     this.search = {max: 256, min: 3};
     this.publications = {items: [], numTotal: 0};
@@ -23,7 +24,24 @@ export default class {
     }
   }
 
+  loadArticles(tabIndex) {
+    $('.search_wrapper_autocomplete').addClass('hidden');
+    this.$state.go('main.articles', {searchQuery: this.searchText, publicationIndex: 0, indicatorIndex: 0, tabIndex});
+  }
+
+  loadPublication(publication) {
+    $('.search_wrapper_autocomplete').addClass('hidden');
+    this.$state.go('main.publication', {id: publication._id});
+  }
+
+  loadIndicator(indicator) {
+    $('.search_wrapper_autocomplete').addClass('hidden');
+    this.$state.go('main.indicator', {id: indicator._id});
+  }
+
   searchTextIsInBounds(searchText) {
     return searchText.length > this.search.min && searchText.length < this.search.max;
   }
+
+
 }
