82bc4f2d104a66b7fb5c6c52df11b3530f7c57b1
GiorgiKochakidze <gkochakidze@gmail.com>
2015-11-16 06:53:53 +0400

    category, date filter

--- client/app/components/main/articles/articles.controller.js
+++ client/app/components/main/articles/articles.controller.js
@@ -1,8 +1,48 @@
 'use strict';
 
 export default class {
-  constructor() {
+  constructor($state, $stateParams, articles, categories) {
     'ngInject';
+    this.$state = $state;
+    this.publications = articles.publications;
+    this.indicators = articles.indicators;
+    this.categories = categories;
+    this.query = $stateParams;
+    this.years = this.getIndicatorsCreationYears();
 
+    this.publications.itemsPerPage = this.publications.items.length;
+    this.publications.currentPage = this.query.publicationsOffset / this.publications.itemsPerPage;
+  }
+
+  changeCategory(category) {
+    if (this.query.categoryId !== category._id) {
+      this.query.categoryId = category._id;
+      this.query.publicationsOffset = 0;
+      this.query.indicatorsOffset = 0;
+      this.$state.go('main.articles', this.query, {reload: true});
+    }
+  }
+
+  changeYear(year) {
+    if (this.query.year !== year) {
+      this.query.year = year;
+      this.query.publicationsOffset = 0;
+      this.query.indicatorsOffset = 0;
+      this.$state.go('main.articles', this.query);
+    }
+  }
+
+  changePublicationPage() {
+    console.log('lsdjfsldkjf')
+    this.query.publicationsOffset = this.publications.currentPage * this.publications.itemsPerPage;
+    this.$state.go('main.articles', this.query);
+  }
+
+  getIndicatorsCreationYears() {
+    var startYear = 2009;
+    var stopYear = (new Date).getFullYear();
+    return _
+      .range(stopYear - startYear + 1)
+      .map((elem, index) => startYear + index);
   }
 }
--- client/app/components/main/articles/articles.js
+++ client/app/components/main/articles/articles.js
@@ -4,14 +4,27 @@ import template from './articles.html!text';
 import controller from './articles.controller';
 import './articles.css!';
 
-export default angular.module('main.articles', [])
+export default angular
+  .module('main.articles', [])
   .config($stateProvider => {
-    $stateProvider
-      .state('articles', {
-        parent: 'main',
-        url: 'articles',
-        template,
-        controller,
-        controllerAs: 'vm'
-      });
+    $stateProvider.state('main.articles', {
+      url: 'articles?searchQuery&categoryId&year&indicatorsOffset&publicationsOffset&tabIndex',
+      template,
+      controller,
+      controllerAs: 'vm',
+      resolve: {
+        categories: CategoryResource => CategoryResource.getCategories(),
+        articles: ($stateParams, ArticleResource) => ArticleResource.searchArticles(parseSearchQuery($stateParams))
+      }
+    });
+
+    function parseSearchQuery($stateParams) {
+      var query = {};
+      if ($stateParams.searchQuery) query.searchQuery = $stateParams.searchQuery;
+      if ($stateParams.categoryId) query.categoryId = $stateParams.categoryId;
+      if ($stateParams.year) query.year = $stateParams.year;
+      if ($stateParams.indicatorsOffset) query.indicatorsOffset = $stateParams.indicatorsOffset;
+      if ($stateParams.publicationsOffset) query.publicationsOffset = $stateParams.publicationsOffset;
+      return query;
+    }
   });
