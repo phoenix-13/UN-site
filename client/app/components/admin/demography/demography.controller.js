5d301b235cdee5e3f1cc5f7067b75fdec1bd0a47
GiorgiKochakidze <gkochakidze@gmail.com>
2015-11-16 14:58:16 +0400

    demography admin save

--- client/app/components/admin/demography/demography.controller.js
+++ client/app/components/admin/demography/demography.controller.js
@@ -8,7 +8,9 @@ export default class {
     this.DemographyResource = DemographyResource;
     this.Toast = Toast;
     this.demographics = {};
-    demographics.forEach(demography => this.demographics[demography.region.geo] = demography);
+    this.indexDemographicsByRegion(demographics);
+    this.indexDemographicsByYear();
+    this.years = _.range(12).map((elem, i) => 2009 + i);
   }
 
   openUpdateDemographyModal(targetEvent, regionName) {
@@ -18,5 +20,29 @@ export default class {
       .then(yearValues => this.DemographyResource.updateDemography(region._id, {yearValues}))
       .then(() => this.Toast.show('Demography Updated Successfully!'))
       .then(() => this.$state.reload());
+  }
+
+  showDemographyInfo(event, demographyRegionName) {
+    if (this.selectedDemography.region.geo !== demographyRegionName) {
+      $('#selectedDemography').removeClass('hidden');
+      this.selectedDemography = this.demographics.filter(demography => demography.region.geo === demographyRegionName)[0];
+      $('#selectedDemography').css({"top": event.pageY + 20, "left": event.pageX - 50});
+    }
+  }
+
+  hideDemographyInfo(demographyRegionName) {
+    if (this.selectedDemography.region.geo !== demographyRegionName) {
+      $('#selectedDemography').addClass('hidden');
+    }
+  }
+
+  indexDemographicsByRegion(demographics) {
+    demographics.forEach(demography => this.demographics[demography.region.geo] = demography);
+  }
+
+  indexDemographicsByYear = () => {
+    _.forIn(this.demographics, (value, key) => {
+      value.valuesMap = _.indexBy(value.values, 'year');
+    });
   }
 }
